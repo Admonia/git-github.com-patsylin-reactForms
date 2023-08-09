1fed521a0636fd4f2c157accb6cdf46387897473
Admonia <dancingshadows309@gmail.com>
2023-08-09 14:55:32 -0400

    experiment

---
+++ react-forms/src/App.jsx
@@ -0,0 +1,10 @@
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+export default function App() {
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
+}
---
+++ react-forms/src/components/SignUpForm.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+export default function SignUpForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Form submitted!");
+    // Add more logic here (e.g., validation, API calls)
+  }
+
+  return (
+    <div>
+      <h2>Sign Up!</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
