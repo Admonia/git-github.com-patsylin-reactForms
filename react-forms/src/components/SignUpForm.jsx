5d3ef4d873fa5183540ea42f03d11809d95567a3
Admonia <dancingshadows309@gmail.com>
2023-08-09 15:54:28 -0400

    second commit

--- react-forms/src/components/SignUpForm.jsx
+++ react-forms/src/components/SignUpForm.jsx
@@ -1,19 +1,39 @@
+// SignUpForm.jsx
 import React, { useState } from "react";
 
-export default function SignUpForm() {
+export default function SignUpForm({ setToken }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
   async function handleSubmit(event) {
     event.preventDefault();
-    console.log("Form submitted!");
-    // Add more logic here (e.g., validation, API calls)
+
+    try {
+      const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      console.log(response);
+
+      if (response.ok) {
+        const result = await response.json();
+        setToken(result.token);
+      } else {
+        setError("Sign-up failed. Please try again.");
+      }
+    } catch (error) {
+      setError("An error occurred. Please try again later.");
+    }
   }
 
   return (
     <div>
       <h2>Sign Up!</h2>
+      {error && <p>Error: {error}</p>}
       <form onSubmit={handleSubmit}>
         <label>
           Username:
